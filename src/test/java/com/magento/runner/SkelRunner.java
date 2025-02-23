af77da863a408a136e21d91cc5f70efa1136ba5f
Chaitanya4803 <pchaitanya7799@gmail.com>
2025-02-23 16:18:59 +0530

    changes on 23/02

--- src/test/java/com/magento/runner/SkelRunner.java
+++ src/test/java/com/magento/runner/SkelRunner.java
@@ -3,9 +3,7 @@ package com.magento.runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/features",
-glue = "com.magento.stepdefinations")
-//tags = "@ForSkeleton"
-public class SkelRunner extends AbstractTestNGCucumberTests{
+@CucumberOptions(features = "src/test/resources/features", glue = "com.magento.stepdefinations", tags = "@ForSkeleton")
+public class SkelRunner extends AbstractTestNGCucumberTests {
 
 }
