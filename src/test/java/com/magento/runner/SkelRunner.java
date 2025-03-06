e8fbdefd0c849a637499dfce3869e5293cd409dc
Chaitanya4803 <pchaitanya7799@gmail.com>
2025-03-06 21:35:14 +0530

    Pom changes

--- src/test/java/com/magento/runner/SkelRunner.java
+++ src/test/java/com/magento/runner/SkelRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 
 @CucumberOptions(features = "src/test/resources/features", glue = "com.magento.stepdefinations", plugin = {
-		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" } , tags = "@Trial")
+		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" }) //, tags = "@Trial")
 public class SkelRunner extends AbstractTestNGCucumberTests {
 
 }
